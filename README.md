35a0743de8ae254db43a29926d16b810b37d05c6
kthfre <golman@kth.se>
2021-02-05 12:53:53 +0100

    client and server added

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # IV1201
 Course project repository
+
+To run, in server directory:
+```
+PORT=3001 npm start
+```
+
+In client directory:
+```
+npm start
+```
---
+++ server/routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+//  res.send('respond with a resource');
+
+    res.json([{id: 1, username: "testuser"}]);
+});
+
+module.exports = router;

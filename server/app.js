b3d77218a07d72a8b511fbcc845b675feb1fb959
Richard F <ralvarez@kth.se>
2021-02-07 19:34:21 +0100

    configure heroku attempt 3

--- server/app.js
+++ server/app.js
@@ -43,7 +43,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  //res.render('error'); // throws error if a view engine is not used
+  res.json({ error: err});
 });
 
 module.exports = app;

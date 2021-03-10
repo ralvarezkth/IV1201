b02e7449b94e00d3457f15a34d9820533db6291c
Richard F <ralvarez@kth.se>
2021-03-10 18:35:42 +0100

    cleanup

--- db-gen/scripts/migrate.js
+++
@@ -1,6 +0,0 @@
-import pg from "pg"
-
-console.log("migrate")
-console.log(process)
-
-let connectionString = "postgres://postgres@localhost:5432/"
--- db-gen/scripts/recruit_original_drop.sql
+++
@@ -1,7 +0,0 @@
-\c recruit_original
-
-drop table competence_profile;
-drop table availability;
-drop table person;
-drop table role;
-drop table competence;

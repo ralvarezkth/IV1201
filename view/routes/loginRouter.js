3d3ccea562d090d241dbdd7f516f684599f4b9a1
Richard F <ralvarez@kth.se>
2021-03-18 16:30:44 +0100

    fix validation usage to use the validated input

--- view/routes/loginRouter.js
+++ view/routes/loginRouter.js
@@ -1,46 +1,45 @@
-var express = require('express');
-const VError = require('verror');
-const { UserCtrl } = require('../../controller');
+var express = require("express");
+const VError = require("verror");
+const { UserCtrl } = require("../../controller");
 var router = express.Router();
-const jwt = require('jsonwebtoken');
-const ValidatorUtil = require('../../util/validatorUtil');
+const jwt = require("jsonwebtoken");
+const ValidatorUtil = require("../../util/validatorUtil");
 
 /* GET /login */
-router.get('/', function(req, res, next) {
-    const username = req.query.username;
-    const password = req.query.password;
+router.get("/", function (req, res, next) {
+  const username = req.query.username;
+  const password = req.query.password;
 
-    const validator = new ValidatorUtil();
-    const validatedUser = validator.validateUserLogin(username, password);
-    if(!validatedUser.error){
-        getUser(username, password)
-        .then(user => {
-            if(user) {
-                jwt.sign({"id": user.id}, 'secretkey', (err, token) =>{
-                    getRole(user.id).then(data => {
-                        user.dataValues.role = data.applicant ? "applicant" : "recruiter";
-                        res.json({user, token})
-                    });
-                });
-            } else {
-                res.status(401).json({error: VError.info(err).message});
-            }
-
-        })
-        .catch(err => {
-            res.status(500).json({error: VError.info(err).message});
-        });
-    }else{
-        res.status(400).json({error: validatedUser.error});
-    }
+  const validator = new ValidatorUtil();
+  const validatedUsername = validator.validateUserLogin(username, password);
+  if (!validatedUsername.error) {
+    getUser(validatedUsername, password)
+      .then(user => {
+        if (user) {
+          jwt.sign({ id: user.id }, "secretkey", (err, token) => {
+            getRole(user.id).then(data => {
+              user.dataValues.role = data.applicant ? "applicant" : "recruiter";
+              res.json({ user, token });
+            });
+          });
+        } else {
+          res.status(401).json({ error: VError.info(err).message });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ error: VError.info(err).message });
+      });
+  } else {
+    res.status(400).json({ error: validatedUsername.error });
+  }
 });
 
 async function getUser(username, password) {
-    return await UserCtrl.getUser(username, password);
+  return await UserCtrl.getUser(username, password);
 }
 
 async function getRole(id) {
-    return await UserCtrl.getRole(id);
+  return await UserCtrl.getRole(id);
 }
 
 module.exports = router;
--- view/routes/registerRouter.js
+++ view/routes/registerRouter.js
@@ -1,28 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const {VError} = require('verror')
-const {UserCtrl} = require('../../controller');
-const UserDTO = require('../../model/dto/userDTO');
-const ValidatorUtil = require('../../util/validatorUtil');
+const { VError } = require("verror");
+const { UserCtrl } = require("../../controller");
+const UserDTO = require("../../model/dto/userDTO");
+const ValidatorUtil = require("../../util/validatorUtil");
 
 /* POST /content - Applicant account creation. */
-router.post('/', function(req, res, next) {
-    let user = req.body.newUser;
-    const validator = new ValidatorUtil();
-    const validatedUser = validator.validateNewUser(user);
-    if (!validatedUser.error) {
-        let userDTO = new UserDTO(null, user.firstName, user.lastName, user.username, user.password, user.email, user.ssn);
-        createUser(userDTO)
-        .then(dat => res.json(dat))
-        .catch(err => {
-            res.status(500).json({error: VError.info(err).message});
-        });
-    }else{
-        res.status(400).json({error: validatedUser.error});    }
+router.post("/", function (req, res, next) {
+  let user = req.body.newUser;
+  const validator = new ValidatorUtil();
+  const validatedUser = validator.validateNewUser(user);
+  if (!validatedUser.error) {
+    let userDTO = new UserDTO(
+      null,
+      validatedUser.firstName,
+      validatedUser.lastName,
+      validatedUser.username,
+      validatedUser.password,
+      validatedUser.email,
+      validatedUser.ssn
+    );
+    createUser(userDTO)
+      .then(dat => res.json(dat))
+      .catch(err => {
+        res.status(500).json({ error: VError.info(err).message });
+      });
+  } else {
+    res.status(400).json({ error: validatedUser.error });
+  }
 });
 
 async function createUser(user) {
-    return await UserCtrl.setUser(user);
+  return await UserCtrl.setUser(user);
 }
 
 module.exports = router;
